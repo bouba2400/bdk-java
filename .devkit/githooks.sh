99ba8cff13e606858fbef02356cc6ed1c31223ff
BD <bd@earth.net.test>
2024-08-15 00:10:11 -0400

    refactor gradle setup

--- .devkit/githooks.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-pwd
-cp ../.cicd/githooks/* ../.git/hooks/ || cp .cicd/githooks/* .git/hooks/
---
+++ tools/shell-scripts/install.deb.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# get script dir
+sdir=$(dirname "$0")
+cwdir=$(pwd)
+
+# stash cwd if different from script dir
+if [ "$sdir" != "$cwdir" ]; then
+  cd "$sdir" || exit 1
+fi
+
+# invoke installers
+. install.stack.deb.sh
+. install.githooks.sh
+
+# restore cwd if different from script dir
+if [ "$sdir" != "$cwdir" ]; then
+  cd "$cwdir" || exit 1
+fi
