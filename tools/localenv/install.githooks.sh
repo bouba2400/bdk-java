ddf76fdcf0ec928fbb7613be1c339e496f3945e3
BD <bd@earth.net.test>
2024-09-01 13:24:50 -0400

    refactor git hooks setup

--- tools/localenv/install.githooks.sh
+++ tools/localenv/install.githooks.sh
@@ -1,20 +1,3 @@
 #!/usr/bin/env sh
 
 git config core.hooksPath ".githooks"
-
-# get script dir
-sdir=$(dirname "$0")
-cwdir=$(pwd)
-
-# stash cwd if different from script dir
-if [ "$sdir" != "$cwdir" ]; then
-  cd "$sdir" || exit 1
-fi
-
-# make hooks executable
-chmod +x ../../.githooks/*
-
-# restore cwd if different from script dir
-if [ "$sdir" != "$cwdir" ]; then
-  cd "$cwdir" || exit 1
-fi
